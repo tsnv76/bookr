cc7c61764d512e845daa966a8d997c543b70802a
tsnv76 <tsnv76@yandex.ru>
2023-09-28 16:21:08 +0700

    commit of 28.09.2023

--- BOOKR/reviews/views.py
+++ BOOKR/reviews/views.py
@@ -1,14 +1,37 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Book
+from .models import Book, Review
+from .utils import average_rating
 
 
 def index(request):
-    name = 'Bob!'
-    return render(request, "base.html", {"name": name})
+    return render(request, 'base.html')
 
-def welcome_view(request):
-    message = f'<html><h1>Welcome to Bookr!</h1!>' \
-              f'<p>{Book.objects.count()} books and counting! </p></html>'
-    return HttpResponse(message)
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request,
+                  "reviews/search-result.html",
+                  {"search_text": search_text})
+
+
+def book_list(request):
+    books = Book.objects.all()
+    books_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        books_list.append({'book': book,
+                           'book_rating': book_rating,
+                           'number_of_reviews': number_of_reviews})
+
+    context = {
+        'book_list': books_list
+    }
+    return render(request, 'reviews/book_list.html', context)
